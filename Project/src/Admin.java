dab1b3e5e86c98c64d79c45eea545a1b8837c126
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-01-14 12:11:50 +0330

    tcp connenction handled

--- Project/src/Admin.java
+++ Project/src/Admin.java
@@ -1,2 +1,95 @@
-public class Admin {
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * Admin is a singleton class
+ */
+public class Admin{
+
+
+    public ArrayList<User> users = new ArrayList<>();
+    private int redundancy = 2;
+
+
+    public Admin() throws IOException {
+        Thread t = new Server();
+        t.start();
+    }
+
+//    public static Admin getInstance() throws IOException{
+//        if (single_instance == null)
+//            single_instance = new Admin();
+//
+//        return single_instance;
+//    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public User getUser(int id){
+        for(User user : users)
+            if (user.getId() == id)
+                return user;
+
+        return null;
+    }
+
+//    public static void main(String[] args) throws IOException{
+//        try {
+//            Thread t = new Server();
+//            t.start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
 }
+
+class Server extends Thread{
+    private ServerSocket serverSocket;
+    private int publicPort = 8888;
+
+
+    public void run(){
+        try {
+            serverSocket = new ServerSocket(publicPort);
+        } catch (IOException e) {}
+
+        while (true){
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {}
+            System.out.println("new Thread Created");
+
+            try {
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                String request = in.readUTF();
+
+                if (request.startsWith("upload")){
+                    String fileDirectory = request.split(" ")[1];
+                    int partition = Integer.parseInt(request.split(" ")[2]);
+
+                    System.out.println("file = " + fileDirectory);
+                    System.out.println("partitions = " + partition);
+                }
+            } catch (IOException e){}
+
+
+
+
+
+            try {
+                socket.close();
+            }
+            catch (IOException e) {}
+        }
+    }
+
+}
+
--- Project/src/User.java
+++ Project/src/User.java
@@ -1,2 +1,49 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+
 public class User {
+    private static final String PATH = "/Desktop/User_Files/";
+    private int id;
+    private String directory;
+    private int portListen;
+    private int portDownload;
+    private double totalSize;
+    private Socket client;
+    private HashSet<String> files = new HashSet<>();
+
+    public User(int id){
+        this.id = id;
+        directory = PATH + id + "/";
+        portListen = 4000 + id;
+        portDownload = 6000 + id;
+        totalSize = 0;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public double getTotalSize(){
+        return totalSize;
+    }
+
+    public void upload(String fileName, int partitions) throws IOException{
+        files.add(fileName);
+        String filePath = directory + fileName;
+        System.out.println("Uploading ...");
+        try {
+            client = new Socket("localhost", 8888);
+        } catch (UnknownHostException e){} catch (IOException e){}
+        System.out.println("User connected");
+
+        DataOutputStream out = new DataOutputStream(client.getOutputStream());
+        String request = "upload " + filePath + " " + partitions;            //e.g. upload /.../readme.txt 3
+        out.writeUTF(request);
+
+
+
+    }
 }
