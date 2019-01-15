c4ba8e0af072f7c1bf9f48dc2bf22d5a7f1680ed
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-01-15 21:17:53 +0330

    Uploading is OK

--- Project/src/User.java
+++ Project/src/User.java
@@ -1,9 +1,8 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
 import java.util.HashSet;
 
 public class User {
@@ -24,6 +23,9 @@ public class User {
         portAllocationReceiver = 5000 + id;
         portDownload = 6000 + id;
         totalSize = id;
+
+        Thread t = new UploadListener(portListen, directory);
+        t.start();
     }
 
     public int getId(){
@@ -56,8 +58,124 @@ public class User {
 
         System.out.println("Allocation received = " + allocationFormat);
 
-        //TODO this part should send file to allocators
+        //TODO this part should send file to allocators -> portListen
+        // upload + filename001 + size -> 400X
 
+        long fileSize = new File(filePath).length();
+        long[] size = new long[partitions];
+        long sum = 0;
+
+        //12,3 -> 4,4,4
+        //11,3 -> 4,4,3
+        //10,3 -> 4,4,2
+        for (int i = 0; i < partitions - 1; i++){
+            size[i] = (long)Math.ceil(fileSize/partitions);
+            sum += size[i];
+        }
+        size[partitions - 1] = fileSize - sum;
+
+        String[] id_part = allocationFormat.split(",");
+        int[] idFixed = new int[id_part.length];
+        int[] partFixed = new int[id_part.length];
+        for (int i = 0; i < id_part.length; i++){
+            idFixed[i] = Integer.parseInt(id_part[i].split(":")[1]);
+            partFixed[i] = Integer.parseInt(id_part[i].split(":")[0]);
+        }
+
+        for (int i = 0; i < id_part.length; i++){  // sending->file size
+            try {
+                Socket socketTmp = new Socket("localhost", 4000 + idFixed[i]);
+                DataOutputStream outTmp = new DataOutputStream(socketTmp.getOutputStream());
+                String info = "upload " + (fileName + "." +  partFixed[i]) + " " + size[partFixed[i]-1];
+                outTmp.writeUTF(info);
+
+                socketTmp.close();
+                outTmp.close();
+            } catch (IOException e){}
+        }
+
+
+        //TODO sending partitions
+
+        File file = new File(directory + fileName);
+        byte[] fileContent = null;
+        try {
+            fileContent = Files.readAllBytes(file.toPath());
+        } catch (IOException e){}
+        for (int i = 0; i < id_part.length; i++) {  // sending partitions
+            Socket socketTmp = new Socket("localhost", 4000 + idFixed[i]);
+            DataOutputStream outTmp = new DataOutputStream(socketTmp.getOutputStream());
+
+            long offset = 0;
+            for(int j = 1; j < partFixed[i]; j++)
+                offset += size[j-1];
+
+            int len = (int)size[partFixed[i]-1];
+            System.out.println("len = " + len);
+            outTmp.write(fileContent, (int)offset, len);
+
+            socketTmp.close();
+            outTmp.close();
+        }
+        System.out.println("All files Sent");
 
     }
+
+}
+
+
+class UploadListener extends Thread{
+    private ServerSocket uploadListenerSocket;
+    private int port;
+    private String directory;
+
+    public UploadListener(int port, String directory){
+        this.port = port;
+        this.directory = directory;
+    }
+
+    public void run(){
+        try {
+            uploadListenerSocket = new ServerSocket(port);
+        } catch (IOException e) {}
+
+        while (true){
+            Socket socketUp = null;
+            try {
+                socketUp = uploadListenerSocket.accept();
+            } catch (IOException e) {}
+            //System.out.println("");
+            try {
+                DataInputStream in = new DataInputStream(socketUp.getInputStream());
+                String request = in.readUTF();
+                if (request.startsWith("upload")){
+                    String fileName = request.split(" ")[1];
+                    long fileSize = Long.parseLong(request.split(" ")[2]);
+
+
+                    System.out.println(port + ": fileSize = " + fileSize);
+                    System.out.println(port + ": fileName = " + fileName);
+
+                    in.close();
+                    socketUp.close();
+                    socketUp = uploadListenerSocket.accept();
+                    byte[] fileContent = new byte[(int)fileSize];
+                    in = new DataInputStream(socketUp.getInputStream());
+
+                    in.readFully(fileContent);
+                    System.out.println(port + " file received");
+
+                    try (FileOutputStream fos = new FileOutputStream(directory + fileName)) {
+                        fos.write(fileContent);
+                    } catch (IOException e){}
+                }
+            } catch (IOException e){}
+
+            try {
+                socketUp.close();
+            }
+            catch (IOException e) {}
+        }
+    }
+
 }
