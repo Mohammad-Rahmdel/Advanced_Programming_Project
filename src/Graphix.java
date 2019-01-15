8b6d72c1aab7ba03f2a9e324a4256be90b65eb91
mmheydari97 <heidarymm@yhoo.com>
2019-01-16 01:37:26 +0330

    Second Try

--- src/Graphix.java
+++ src/Graphix.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class Graphix {
     private JFrame frame;
@@ -23,6 +22,7 @@ public class Graphix {
     private JMenu uploadMenu;
     private JMenu toolsMenu;
     private JMenu helpMenu;
+    private JPanel propertiesPanel;
     private JLabel labelName;
     private JTextField nameField;
     private JLabel labelExt;
@@ -31,6 +31,16 @@ public class Graphix {
     private JTextField sizeField;
     private JLabel labelParts;
     private JTextField partsField;
+    private JLabel labelDist;
+    private JTextField distField;
+    private JLabel labelOwner;
+    private JTextField ownerField;
+    private JLabel labelCreateDate;
+    private JTextField createDateField;
+    private JLabel labelAccess;
+    private JTextField accessField;
+    private JMenuItem fiRefresh;
+    private JMenuItem fiExit;
 
     public Graphix(){
         frame = new JFrame();
@@ -47,18 +57,35 @@ public class Graphix {
         nameField.setEditable(false);
 
         labelExt = new JLabel("File extention: ");
-        extField = new JTextField("test.png");
+        extField = new JTextField("png");
         extField.setEditable(false);
 
         labelSize = new JLabel("Size: ");
-        sizeField = new JTextField("test.png");
+        sizeField = new JTextField("1.5 MB");
         sizeField.setEditable(false);
 
         labelParts = new JLabel("Partition: ");
-        partsField = new JTextField("test.png");
+        partsField = new JTextField("2");
         partsField.setEditable(false);
 
-        JPanel propertiesPanel = new JPanel();
+        labelDist = new JLabel("File node distribution: ");
+        distField = new JTextField("[1:2, 2:2]");
+        distField.setEditable(false);
+
+        labelOwner = new JLabel("Owner: ");
+        ownerField = new JTextField("Ali");
+        ownerField.setEditable(false);
+
+        labelCreateDate = new JLabel("Created: ");
+        createDateField = new JTextField("2018-12-05");
+        createDateField.setEditable(false);
+
+        labelAccess = new JLabel("Last accessed: ");
+        accessField = new JTextField("2018-12-10");
+        accessField.setEditable(false);
+
+        propertiesPanel = new JPanel(new GridLayout(8,2));
+        propertiesPanel.setMaximumSize(new Dimension(200,500));
         propertiesPanel.add(labelName);
         propertiesPanel.add(nameField);
         propertiesPanel.add(labelExt);
@@ -67,6 +94,16 @@ public class Graphix {
         propertiesPanel.add(sizeField);
         propertiesPanel.add(labelParts);
         propertiesPanel.add(partsField);
+        propertiesPanel.add(labelParts);
+        propertiesPanel.add(partsField);
+        propertiesPanel.add(labelDist);
+        propertiesPanel.add(distField);
+        propertiesPanel.add(labelOwner);
+        propertiesPanel.add(ownerField);
+        propertiesPanel.add(labelCreateDate);
+        propertiesPanel.add(createDateField);
+        propertiesPanel.add(labelAccess);
+        propertiesPanel.add(accessField);
 
         fileTabs = new JTabbedPane();
         filePanel.add(fileTabs, BorderLayout.CENTER);
@@ -76,7 +113,7 @@ public class Graphix {
         propertiesTab = new JPanel(new BorderLayout());
         propertiesTab.setName("Properties");
 
-        propertiesTab.add(propertiesPanel, BorderLayout.CENTER);
+        propertiesTab.add(propertiesPanel, BorderLayout.WEST);
 
 
         fileTabs.add(previewTab);
@@ -105,7 +142,39 @@ public class Graphix {
         });
 
         duplicateButton = new JButton("Duplicate");
+        duplicateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JDialog dialog = new JDialog(frame, "Duplication Result");
+                dialog.add(new JLabel("Done!"));
+                dialog.setLayout(new FlowLayout());
+                dialog.setSize(200,100);
+                dialog.setVisible(true);
+            }
+        });
         renameButton = new JButton("Rename");
+        renameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JDialog dialog = new JDialog(frame, "Rename File");
+                dialog.setLayout(new BorderLayout());
+                dialog.add(new JLabel("new Name:"), BorderLayout.WEST);
+                JTextField newName = new JTextField();
+                newName.setPreferredSize(new Dimension(200,25));
+                dialog.add(newName, BorderLayout.EAST);
+                JButton submitBtn = new JButton("Submint");
+                dialog.add(submitBtn, BorderLayout.SOUTH);
+                submitBtn.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        System.out.println(newName.getText());
+                    }
+                });
+                dialog.setSize(400,100);
+                dialog.setVisible(true);
+
+            }
+        });
         deleteButton = new JButton("Delete");
         buttonsPanel = new JPanel(new FlowLayout());
         buttonsPanel.add(downloadButton);
@@ -115,15 +184,18 @@ public class Graphix {
         filePanel.add(buttonsPanel, BorderLayout.SOUTH);
 
         filesList = new JList();
+        String[] listOfFiles = new String[]{"Ubuntu.iso", "Movie.mp4"};
+        filesList.setListData(listOfFiles);
+        //filesList.add
         listPanel.add(filesList);
         scrollBar1 = new JScrollBar();
         listPanel.add(scrollBar1, BorderLayout.WEST);
 
         topMenu = new JMenuBar();
         fileMenu =  new JMenu("File");
-        JMenuItem fiRefresh = new JMenuItem("Refresh List");
+        fiRefresh = new JMenuItem("Refresh List");
         fileMenu.add(fiRefresh);
-        JMenuItem fiExit = new JMenuItem("Exit Application");
+        fiExit = new JMenuItem("Exit Application");
         fileMenu.add(fiExit);
 
         topMenu.add(fileMenu);
