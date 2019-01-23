8c20bea31340cd57e0e1112da6831ab797aaa58d
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-01-23 22:51:17 +0330

    Admin class comments added

--- Project/src/Admin.java
+++ Project/src/Admin.java
@@ -3,41 +3,48 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 
 /**
- * Admin is a singleton class
+ * Class Admin
+ * this class manages all the connections between peers
+ * all of the peers send their requests to this class
  */
 public class Admin{
 
 
-    public static ArrayList<User> users = new ArrayList<>();
-    private static ArrayList<String[]> files = new ArrayList<>(); // [allocation format][file name][id of uploader][directory]
-    private static int redundancy = 2;
+    public static ArrayList<User> users = new ArrayList<>();        // stores users
+    private static ArrayList<String[]> files = new ArrayList<>();   // stores [allocation format][file name][id of uploader][directory]
+    private static int redundancy = 2;                              // how many backups should the network have
 
 
     public Admin(){
         Thread t = new Server(this);
-        t.start();
+        t.start();                              // starts listening to the requests
     }
-    //    public static Admin getInstance() throws IOException{
-//        if (single_instance == null)
-//            single_instance = new Admin();
-//
-//        return single_instance;
-//    }
 
-
+    /**
+     *
+     * @return all of the files in the network
+     */
     public ArrayList<String[]> getFiles(){
         return files;
     }
 
 
+    /**
+     *
+     * @param user when a new user enter the network
+     */
     public void addUser(User user){
         users.add(user);
     }
 
+    /**
+     * is the peer still in the network or not
+     * @param id
+     * @return
+     */
     public static boolean userExists(int id){
         for (User u : users)
             if (u.getId() == id)
@@ -46,6 +53,11 @@ public class Admin{
         return false;
     }
 
+    /**
+     *
+     * @param id
+     * @return object of the user according to its id
+     */
     public User getUser(int id){
         for(User user : users)
             if (user.getId() == id)
@@ -54,6 +66,11 @@ public class Admin{
         return null;
     }
 
+    /**
+     *
+     * @param fileName
+     * @return whether fileName is in the network files or not
+     */
     public boolean hasFile(String fileName){
         for (String[] x : files){
             if (x[1].equals(fileName))
@@ -63,18 +80,23 @@ public class Admin{
     }
 
 
+    /**
+     * this method return the allocation format for downloading the input file name
+     * @param fileName
+     * @return
+     */
     public static String downloadManager(String fileName){
         String allocation = "";
         for(String[] x: files) {
             if (x[1].equals(fileName))
                 allocation = x[0];
         }
-        System.out.println("alloc = " + allocation);
+        //System.out.println("alloc = " + allocation);
         String[] split = allocation.split(",");
         int numberOfParts = split.length / redundancy;
         int cnt = 0;
         String downloadFormat = "";
-        System.out.println("N = " + numberOfParts);
+        //System.out.println("N = " + numberOfParts);
         for (int i = 1; i <= numberOfParts; i++){
             boolean flag = true;
             for (String y : split){
@@ -82,20 +104,27 @@ public class Admin{
                     if(userExists(Integer.parseInt(y.split(":")[1]))){
                         downloadFormat += y + ",";
                         cnt++;
-                        flag = false;
+                        flag = false;                                         //file found
                     }
                 }
             }
         }
 
-        downloadFormat = downloadFormat.substring(0,downloadFormat.length() - 1);
+        downloadFormat = downloadFormat.substring(0,downloadFormat.length() - 1);   //omits the last ','
         if (cnt == numberOfParts)
             return "" + numberOfParts + "#" + downloadFormat;   // e.g. 3#1:2,2:3,3:5
+        // this indicates that our files has three part the peer number 2 should send part one and so on.
         else
             return "failed";
     }
 
 
+    /**
+     *
+     * @param id = id who has made the request
+     * @param name = the file which has been requested
+     * @return the accessibility situation
+     */
     public int hasAccess(int id, String name){
         int status = 1; // Wrong file name
         int foundId = -1;
@@ -113,32 +142,40 @@ public class Admin{
         return status;
     }
 
+    /**
+     *
+     * @param path = path of the file
+     * @param n = partition number
+     * @param id = id of the peer
+     * @return the allocation format
+     * @throws FileNotFoundException
+     */
     public static String allocation(String path, int n, String id) throws FileNotFoundException {
         String[] ss = path.split("/");
         String directory = "";
         for(int i = 0; i < ss.length - 1; i++){
-            directory += ss[i] + "/";
+            directory += ss[i] + "/";               //removes the name of the file from path
         }
 
         int splitter = ss.length;
         String fileName = ss[splitter-1];
         String idUploader = id;//ss[splitter-2];
 
-        System.out.println("path = " + path);
-        System.out.println("name = " + fileName);
-        System.out.println("ID = " + idUploader);
+        //System.out.println("path = " + path);
+        //System.out.println("name = " + fileName);
+        //System.out.println("ID = " + idUploader);
 
         long fileSize = new File(path).length();
-        System.out.println("size of file = " + fileSize);
+        //System.out.println("size of file = " + fileSize);
 
 
         String allocation = "";
-        ArrayList<User> sortedUsersBySize = sortUsers(users);
+        ArrayList<User> sortedUsersBySize = sortUsers(users);       //sort the users by their total storage
         int usersNo = sortedUsersBySize.size();
 
         int index = 0;
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= redundancy; j++){
+        for(int i = 1; i <= n; i++){                                //n partitions
+            for(int j = 1; j <= redundancy; j++){                   //redundancy for the j_th part
                 if(index == sortedUsersBySize.size())
                     index = 0;
                 allocation += (i + ":" + sortedUsersBySize.get(index).getId() + ",");
@@ -158,6 +195,11 @@ public class Admin{
     }
 
 
+    /**
+     * this method removes the file from admin list
+     * and deletes all of the partitios of the file from the network
+     * @param fileName
+     */
     public void delete(String fileName){
         int y = 0;
         String[] info = null;
@@ -165,14 +207,14 @@ public class Admin{
             if(x[1].equals(fileName)){
                 info = x;
                 files.remove(y);
-                System.out.println("removed from admin");
+                //System.out.println("removed from admin");
                 break;
             }
             y++;
         }
         String[] id_part = info[0].split(",");
-        int[] idFixed = new int[id_part.length];
-        int[] partFixed = new int[id_part.length];
+        int[] idFixed = new int[id_part.length];        //peer with ID=id who has a partition of the file
+        int[] partFixed = new int[id_part.length];      //which part the peer has
         for (int i = 0; i < id_part.length; i++){
             idFixed[i] = Integer.parseInt(id_part[i].split(":")[1]);
             partFixed[i] = Integer.parseInt(id_part[i].split(":")[0]);
@@ -190,6 +232,12 @@ public class Admin{
         }
     }
 
+    /**
+     * this method rename the file in admin's list
+     * it also renames all the partitions in peers
+     * @param fileName = old name
+     * @param newName = new name
+     */
     public void rename(String fileName, String newName){
         String[] info = null;
         for (String[] x: files){
@@ -221,6 +269,10 @@ public class Admin{
 
 }
 
+/**
+ * the admin listens to all the request on port 8888
+ * this connection is always up
+ */
 class Server extends Thread{
     private ServerSocket serverSocket;
     private int publicPort = 8888;
@@ -246,7 +298,7 @@ class Server extends Thread{
                 DataInputStream in = new DataInputStream(socket.getInputStream());
                 String request = in.readUTF();
 
-                if (request.startsWith("upload")){
+                if (request.startsWith("upload")){          //if upload request received
                     String fileDirectory = request.split(" ")[1];
                     int partition = Integer.parseInt(request.split(" ")[2]);
 
@@ -255,7 +307,7 @@ class Server extends Thread{
                     String allocation = Admin.allocation(fileDirectory, partition, idUploader);
 
 
-                    int portAllocation = 5000 + Integer.parseInt(idUploader);
+                    int portAllocation = 5000 + Integer.parseInt(idUploader);   //sends response to requester
 
                     System.out.println(allocation);
                     Socket allocationSender = new Socket("localhost", portAllocation);
@@ -290,7 +342,7 @@ class Server extends Thread{
                     }
 
                 }
-                else if(request.startsWith("send_list")){
+                else if(request.startsWith("send_list")){       //when the refresh list button is clicked
                     int portN = 13000 + Integer.parseInt(request.split(" ")[1]);
                     Socket socketList = null;
                     DataOutputStream outList = null;
